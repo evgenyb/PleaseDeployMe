35834bc2f3420b2fccd3b723f6accda4c401866f
Evgeny Borzenin <evgeny.borzenin@if.no>
2015-07-13 15:32:25 +0200

    Added rh migration database script

--- nuget/content/build/lib/rh-migrate-database.ps1
+++ nuget/content/build/lib/rh-migrate-database.ps1
@@ -1,29 +1,65 @@
 # Create new Database
 Properties {
+    $executeRemotely = $false
+    # Database connection string
     $connectionString = $null
+    # The database name
+    $databaseName = $null
+    # Full path where scripts are located
     $installationDirectoryPath = $null
+    # Username (only used when $executeRemotely is $true)
     $username = $null
+    # Password (only used when $executeRemotely is $true)
     $password = $null
 }
 
-$roundhouse_exe_path = $installationDirectoryPath + "\rh.exe"
-$scripts_dir = $installationDirectoryPath + "\Scripts"
-$roundhouse_output_dir = $installationDirectoryPath + "\output"
-$createDbScript = $scripts_dir + "\CreateDB\CreateDatabase.sql"
+Task Default -depends MigrateDatabase
 
-$securePassword = $password | ConvertTo-SecureString -AsPlainText -Force
-$credential =  New-Object -typename System.Management.Automation.PSCredential -ArgumentList $username, $securePassword
+Task MigrateDatabase {
+    Validate-Parameter $connectionString -parameterName "Connection string"
+    Validate-Parameter $installationDirectoryPath -parameterName "Installation directory path"
+    Validate-Parameter $username -parameterName "Username"
+    Validate-Parameter $password -parameterName "Password"
+    Validate-Parameter $databaseName -parameterName "Database name"
 
-Invoke-Command -Credential $credential -ComputerName $env:ComputerName -Authentication Credssp -ArgumentList $roundhouse_exe_path, $connectionString, $scripts_dir, $roundhouse_output_dir, $createDbScript -ScriptBlock {
-    param($roundhouse_exe_path, $connectionString, $scripts_dir, $roundhouse_output_dir, $createDbScript)
-    $params = @("-c", "$connectionString", "-f", "$scripts_dir", "-o", "$roundhouse_output_dir","-cds", $createDbScript , "--env", "OCTOPUS", "--silent")
+    $roundhouse_exe_path = "$installationDirectoryPath\rh.exe"
+    $scripts_dir = "$installationDirectoryPath\Scripts"
+    $roundhouse_output_dir = "$installationDirectoryPath\output"
+    $createDbScript = "$scripts_dir\CreateDB\CreateDatabase.sql"
+    $securePassword = $password | ConvertTo-SecureString -AsPlainText -Force
+    $credential =  New-Object -typename System.Management.Automation.PSCredential -ArgumentList $username, $securePassword
 
-    & $roundhouse_exe_path $params
-
-    if ($LastExitCode -ne 0)
-    {        
-        throw "database deployment failed !!"
-        exit 1
+    if ($executeRemotely -eq $false) {
+        Write-Host "Running in local mode"
+        exec { & $roundhouse_exe_path -d $databaseName -c $connectionString -f $scripts_dir -cds $createDbScript -o $roundhouse_output_dir --silent }
+    } else {
+        Write-Host "Running in remote mode"
+        Invoke-Command -Credential $credential -ComputerName $env:ComputerName -Authentication Credssp -ArgumentList $roundhouse_exe_path, $databaseName, $connectionString, $scripts_dir, $roundhouse_output_dir, $createDbScript -ScriptBlock {
+            param($roundhouse_exe_path, $databaseName, $connectionString, $scripts_dir, $roundhouse_output_dir, $createDbScript)
+            $params = @("-d", "$databaseName", "-c", "$connectionString", "-f", "$scripts_dir", "-o", "$roundhouse_output_dir","-cds", $createDbScript , "--env", "OCTOPUS", "--silent")
+    
+            & $roundhouse_exe_path $params
+    
+            if ($LastExitCode -ne 0)
+            {        
+                throw "Database deployment failed !!"
+                exit 1
+            }
+            exit 0
+        }
     }
-    exit 0
+}
+
+## --------------------------------------------------------------------------------------
+## Helpers
+## --------------------------------------------------------------------------------------
+# Helper for validating input parameters
+function Validate-Parameter([string]$foo, [string[]]$validInput, $parameterName) {
+    IF (! $parameterName -contains "Password")
+    {
+        Write-Host "${parameterName}: $foo"
+    }
+    if (! $foo) {
+        throw "No value was set for $parameterName, and it cannot be empty"
+    }
 }
